ed4e97cda6d4819ca9157b1d96fdfd8f1a1fcc9d
swisskala <80791120+swisskala@users.noreply.github.com>
2023-10-24 19:51:44 +0200

    Create replace.sh

---
+++ replace.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Copy the .bashrc from the current directory (cloned repo) to the user's home directory
+cp .bashrc ~/
+
+if [ $? -eq 0 ]; then
+    echo "Successfully replaced .bashrc with the version from the current directory."
+else
+    echo "Failed to replace .bashrc."
+fi
